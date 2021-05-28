e61ad3df0792c3df0924ca7ab8547afe824b2049
YogPanjarale <yog.panjarale@gmail.com>
2021-05-28 19:47:09 +0530

    webscaping2 edited

--- webscraping2.py
+++ webscraping2.py
@@ -1,68 +1,43 @@
-import time
-import csv
-import os
-from bs4 import BeautifulSoup
-from selenium import webdriver
+from bs4 import BeautifulSoup as bs
 import requests
+import pandas as pd
 
-# url to scrape data from
-target_url = "https://en.wikipedia.org/wiki/List_of_brown_dwarfs"
 
-# getting the chromedriver
-# browser = webdriver.Chrome(os.getcwd()+"\\chromedriver.exe")
-# browser.get(target_url)
-stars_list=[]
-headers = ['Name','Distance','Mass','Radius']
-def scrape():
-    html_page = requests.get(target_url)
-    soup = BeautifulSoup(html_page.content,"html.parser")
-    table_content :soup = soup.select('#mw-content-text > div.mw-parser-output > table:nth-child(14) > tbody')[0]
-    tr:soup
-    # print(soup)
-    # with open('soup.html','w',encoding='utf-8') as f:
-    #     f.write(str(table_content))
-    #     f.close()
-    for tr in table_content.find_all('tr')[1:]:
-        td:soup
-        # print(tr)
-        i=0
-        temp_list=[]
-        for td in tr.find_all('td'):
-            #star name, radius, mass and distance data
-            #Name
-            if i==0 and len(td.find_all('a'))>0:
-                a:soup=td.find_all('a')[0]
-                temp_list.append(str(a.contents[0]))
-            #radius
-            if i==9:
-                c= td.contents
-                if len(c)>0:
-                    temp_list.append(str(c[0]))
-                else:
-                    temp_list.append('-')
-            #mass
-            if i==8:
-                c= td.contents
-                if len(c)>0:
-                    temp_list.append(str(c[0]))
-                else:
-                    temp_list.append('-')
-            #distance light years
-            if i==5:
-                c= td.contents
-                if len(c)>0:
-                    temp_list.append(str(c[0]))
-                else:
-                    temp_list.append('-')
-            i+=1
-        stars_list.append(temp_list)
-        
+url = 'https://en.wikipedia.org/wiki/List_of_brown_dwarfs'
 
-if __name__ =="__main__":
-    scrape()
-    # print(stars_list)
-    with open("dwarfs_stars.csv","w",encoding="utf-8",newline='') as f:
-            w = csv.writer(f)
-            w.writerow(headers)
-            w.writerows(stars_list)
-            f.close()
+page = requests.get(url)
+print(page)
+
+soup = bs(page.text,'html.parser')
+
+star_table = soup.find_all('table')
+print(len(star_table))
+
+
+temp_list= []
+table_rows = star_table[4].find_all('tr')
+for tr in table_rows:
+    td = tr.find_all('td')
+    row = [i.text.rstrip() for i in td]
+    temp_list.append(row)
+print(temp_list)
+
+
+
+Star_names = []
+Distance =[]
+Mass = []
+Radius =[]
+
+
+for i in range(1,len(temp_list)):
+    
+    Star_names.append(temp_list[i][0])
+    Distance.append(temp_list[i][5])
+    Mass.append(temp_list[i][7])
+    Radius.append(temp_list[i][8])
+
+df2 = pd.DataFrame(list(zip(Star_names,Distance,Mass,Radius,)),columns=['Star_name','Distance','Mass','Radius'])
+print(df2)
+
+df2.to_csv('dwarf_stars.csv')
