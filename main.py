6b9750d0d9b7d28f613f7b3ec0bbfb4fb4ed87c7
YogPanjarale <yog.panjarale@gmail.com>
2021-05-28 19:44:33 +0530

    bright_stars.csv

--- main.py
+++ main.py
@@ -1,51 +1,40 @@
-import time
-import csv
-import os
-from bs4 import BeautifulSoup
-from selenium import webdriver
+from bs4 import BeautifulSoup as bs
 import requests
+import pandas as pd
 
-# url to scrape data from
-target_url = "https://en.wikipedia.org/wiki/List_of_brightest_stars_and_other_record_stars"
 
-# getting the chromedriver
-# browser = webdriver.Chrome(os.getcwd()+"\\chromedriver.exe")
-# browser.get(target_url)
-stars_list=[]
-headers = ['Name','Distance','Mass','Radius']
-def scrape():
-    html_page = requests.get(target_url)
-    soup = BeautifulSoup(html_page.content,"html.parser")
-    table_content :soup = soup.find_all("tbody")[0]
-    tr:soup
-    for tr in table_content.find_all("tr"):
-        td:soup
-        i=0
-        temp_list=[]
-        for td in tr.find_all('td'):
-            if i==1 and len(td.find_all('a'))>0:
-                a:soup=td.find_all("a")[0]
-                temp_list.append(str(a.contents[0]))
-            if i==3:
-                if "span" in str(td.contents[0]).strip('\n'):
-                    temp_list.append(str(td.contents[1]).strip('\n'))
-                else:
-                    temp_list.append(str(td.contents[0]).strip('\n'))
-            if i==5:
-                temp_list.append(str(td.contents[0]).strip('\n'))
-            if i==6:
-                temp_list.append(str(td.contents[0]).strip('\n'))
-            i+=1
-            # print(td.contents[0])
-        if len(temp_list)==4:
-            stars_list.append(temp_list)
-        
+bright_stars_url = 'https://en.wikipedia.org/wiki/List_of_brightest_stars_and_other_record_stars'
 
-if __name__ =="__main__":
-    scrape()
-    print(stars_list)
-    with open("planet_data.csv","w",encoding="utf-8",newline='') as f:
-            w = csv.writer(f)
-            w.writerow(headers)
-            w.writerows(stars_list)
-            f.close()
+page = requests.get(bright_stars_url)
+print(page)
+
+soup = bs(page.text,'html.parser')
+
+star_table = soup.find('table')
+
+temp_list= []
+table_rows = star_table.find_all('tr')
+for tr in table_rows:
+    td = tr.find_all('td')
+    row = [i.text.rstrip() for i in td]
+    temp_list.append(row)
+
+
+
+Star_names = []
+Distance =[]
+Mass = []
+Radius =[]
+Lum = []
+
+for i in range(1,len(temp_list)):
+    Star_names.append(temp_list[i][1])
+    Distance.append(temp_list[i][3])
+    Mass.append(temp_list[i][5])
+    Radius.append(temp_list[i][6])
+    Lum.append(temp_list[i][7])
+    
+df2 = pd.DataFrame(list(zip(Star_names,Distance,Mass,Radius,Lum)),columns=['Star_name','Distance','Mass','Radius','Luminosity'])
+print(df2)
+
+df2.to_csv('bright_stars.csv')
